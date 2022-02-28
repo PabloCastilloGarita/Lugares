8404dce0b15c2128a8be344c2be7b85dcd971081
PabloCastilloGarita <pcastillogarita@gmail.com>
2022-02-28 15:01:07 -0600

    Semana05: implementacion de botones y funcionalidades de apps externas

--- app/src/main/java/com/lugares/ui/lugar/UpdateLugarFragment.kt
+++ app/src/main/java/com/lugares/ui/lugar/UpdateLugarFragment.kt
@@ -1,6 +1,10 @@
 package com.lugares.ui.lugar
 
+import android.Manifest
 import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
@@ -34,14 +38,106 @@ class UpdateLugarFragment : Fragment() {
         binding.etCorreo.setText(args.lugar.correo)
         binding.etTelefono.setText(args.lugar.telefono)
         binding.etWeb.setText(args.lugar.web)
+        binding.tvLatitud.text=args.lugar.latitud.toString()
+        binding.tvLongitud.text=args.lugar.longitud.toString()
+        binding.tvAltura.text=args.lugar.altura.toString()
 
-         binding.btUpdateLugar.setOnClickListener() {
-             actualizarLugar()
-         }
+         binding.btUpdateLugar.setOnClickListener() { actualizarLugar() }
+         binding.btEmail.setOnClickListener() { enviarCorreo() }
+         binding.btPhone.setOnClickListener() { hacerLlamada() }
+         binding.btWhatsapp.setOnClickListener() { enviarWhatsApp() }
+         binding.btLocation.setOnClickListener() { verMapa() }
 
         setHasOptionsMenu(true)//este fragmento debe tener un menu adicional
 
         return binding.root
+    }
+
+    private fun verMapa() {
+        val latitud = binding.tvLatitud.text.toString().toDouble() //se extrae la latitud y se pasa a double
+        val longitud = binding.tvLongitud.text.toString().toDouble() //se extrae la longitud y se pasa a double
+
+        if (latitud.isFinite() && longitud.isFinite()) { //podemos usar el recurso...
+            val location = Uri.parse("geo:$latitud,$longitud?z=18")
+            val intent = Intent(Intent.ACTION_VIEW,location) //se ve le mapa desde el app
+
+            startActivity(intent) // se abre el visor de mapas desde el lugar
+
+        } else { //no podemos usar el recurso...
+            Toast.makeText(requireContext(),getString(R.string.msg_datos),Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun verWeb() {
+        val sitio = binding.etWeb.text.toString() //se extrae el sitio web del lugar
+        if (sitio.isNotEmpty()) { //podemos usar el recurso...
+            val web = Uri.parse("http://$sitio")
+            val intent = Intent(Intent.ACTION_VIEW,web) //se va al sitio web desde el app
+
+            startActivity(intent) // se hace el visor web y se muestra el sitio web
+
+        } else { //no podemos usar el recurso...
+            Toast.makeText(requireContext(),getString(R.string.msg_datos),Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun enviarWhatsApp() {
+        val telefono = binding.etTelefono.text.toString() //se extrae el numero de telefono del lugar
+        if (telefono.isNotEmpty()) { //podemos usar el recurso...
+            val intent = Intent(Intent.ACTION_VIEW) //se va a enviar algo desde el app
+            val uri = "whatsapp://send?phone=506$telefono&text="+ //con ese prefijo se sabe que es un numero de telefono
+                getString(R.string.msg_saludos)
+
+            //se establece el app a usar
+            intent.setPackage("com.whatsapp")
+            intent.data = Uri.parse(uri) //se carga la info
+            startActivity(intent)
+
+        } else { //no podemos usar el recurso...
+            Toast.makeText(requireContext(),getString(R.string.msg_datos),Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun hacerLlamada() {
+        val telefono = binding.etTelefono.text.toString() //se extrae el numero de telefono del lugar
+        if (telefono.isNotEmpty()) { //podemos usar el recurso...
+            val intent = Intent(Intent.ACTION_CALL) //se va a enviar algo desde el app
+            intent.data = Uri.parse("tel:$telefono") //con ese prefijo se sabe que es un numero de telefono
+
+            //se procede a validar si hay permisos para hacer la llamada
+
+            if (requireActivity().checkSelfPermission(Manifest.permission.CALL_PHONE) !=
+                    PackageManager.PERMISSION_GRANTED) { //si no tenemos los permisos hay que solicitarlos
+
+                requireActivity().requestPermissions(arrayOf(Manifest.permission.CALL_PHONE),105)
+            } else { // si se han otorgado los permisos
+                requireActivity().startActivity(intent) //se hace la llamada telefonica
+            }
+        } else { //no podemos usar el recurso...
+            Toast.makeText(requireContext(),getString(R.string.msg_datos),Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun enviarCorreo() {
+        val correo = binding.etCorreo.text.toString() //se extrae la cuenta de correo del lugar
+        if (correo.isNotEmpty()) { //podemos usar el recurso...
+            val intent = Intent(Intent.ACTION_SEND) //se va a enviar algo desde el app
+            intent.type = "message/rfc822" //se va a enviar un correo electronico
+
+            //se define el destinatario
+            intent.putExtra(Intent.EXTRA_EMAIL,arrayOf(correo))
+
+            //se define el asunto
+            intent.putExtra(Intent.EXTRA_SUBJECT,getString(R.string.msg_saludos)+" "+binding.etNombre.text)
+
+            //se define el cuerpo del correo inicial
+            intent.putExtra(Intent.EXTRA_TEXT,getString(R.string.msg_mensaje_correo))
+
+            //se solicita el recurso de correo para que se envie este
+            startActivity(intent)
+        } else { //no podemos usar el recurso...
+            Toast.makeText(requireContext(),getString(R.string.msg_datos),Toast.LENGTH_LONG).show()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
